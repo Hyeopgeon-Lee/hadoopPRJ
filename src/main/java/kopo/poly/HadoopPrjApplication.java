472283d47f6e94540d4fc5b2274829291db9efd7
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2022-11-19 12:07:11 +0900

    log4j 수정

--- src/main/java/kopo/poly/HadoopPrjApplication.java
+++ src/main/java/kopo/poly/HadoopPrjApplication.java
@@ -1,9 +1,6 @@
 package kopo.poly;
 
 import kopo.poly.component.impl.Exam01;
-import kopo.poly.component.impl.Exam02;
-import kopo.poly.component.impl.Exam03;
-import kopo.poly.component.impl.Exam04;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.boot.CommandLineRunner;
@@ -17,11 +14,11 @@ public class HadoopPrjApplication implements CommandLineRunner {
 
     private final Exam01 exam01;
 
-    private final Exam02 exam02;
-
-    private final Exam03 exam03;
-
-    private final Exam04 exam04;
+//    private final Exam02 exam02;
+//
+//    private final Exam03 exam03;
+//
+//    private final Exam04 exam04;
 
     @Override
     public void run(String... args) throws Exception {
@@ -31,11 +28,11 @@ public class HadoopPrjApplication implements CommandLineRunner {
         log.info("첫번째 실습");
         exam01.doExam();
 
-        log.info("두번째 실습");
-        exam02.doExam();
-
-        log.info("세번째 실습");
-        exam03.doExam();
+//        log.info("두번째 실습");
+//        exam02.doExam();
+//
+//        log.info("세번째 실습");
+//        exam03.doExam();
 //
 //        log.info("네번째 실습");
 //        exam04.doExam();
